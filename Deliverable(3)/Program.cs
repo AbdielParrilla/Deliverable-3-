c5c07ba1e1cd11a00db7c5eb430abf3eeedf372a
abdie <abdie@LAPTOP-DRGQBN1N>
2023-01-31 17:00:42 -0500

    Add project files.

---
+++ Deliverable(3)/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Deliverable_3_
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {   
+            
+        
+
+            int input = 0;
+            string series = "";
+            
+
+            while (true)
+            {
+                Console.WriteLine("Enter Integer numner between 1 and 100");
+                
+                input = int.Parse(Console.ReadLine());
+
+                if (input >= 1 && input <= 100)
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Specify the series type: Even or Odd: ");
+                series = Console.ReadLine();
+
+                if (series == "Even" || series == "Odd") ;
+                {
+                    break;
+                }
+            }
+
+
+
+            
+
+
+
+            if (series == "Odd")
+            {
+
+                Console.WriteLine("You have selected " + series + " series, the numbers between 0 and "
+                    + input + " are:");
+                for (int i = 1; i <= input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            if (series == "Even")
+            {
+
+                Console.WriteLine("You have selected " + series + " series, the numbers between 0 and " + 
+                    input + " are:");
+                for (int i = 0; i <= input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+        
+        
+
+        
+    }
+}
